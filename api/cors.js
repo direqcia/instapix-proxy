3d9f216ab9487fe14bf9ed1d94cc7c06de0666ad
direqcia <george.sibashvili@gmail.com>
2025-08-20 13:09:21 +0400

    Delete api/cors.js

--- api/cors.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-  const upstream = 'http://api.instapix.ge:29998/Stat';
-  const r = await fetch(upstream);
-  const data = await r.json();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).json(data);
-}
